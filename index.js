fd1ca263b90d8169a90b8032bfd07cb27f1ba892
Pro_Soban <92185551+SobanSheikh@users.noreply.github.com>
2022-12-21 13:37:44 +0500

    1st commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose")
+mongoose.set('strictQuery', true);
+const http = require("http");
+const {Server} =  require("socket.io");
+const cors = require("cors");
+const bodyParser = require("body-parser")
+const router = require('./router');
+const dotenv = require("dotenv");
+dotenv.config()
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
+app.use("/",router);
+const server = http.createServer(app);
+const io = new Server(server,{
+    cors:{
+        origin:true,
+        methods:["GET","POST"]
+    }
+});
+
+io.on("connection",(socket)=>
+    {
+        console.log('Users:: ${socket.id}')
+        socket.on("send_message",(data)=>{
+            socket.broadcast.emit("receive_message",data)
+            
+        })
+
+    })
+server.listen(process.env.PORT || 5000,()=>{ 
+  console.log("Server is running", process.env.PORT);
+})
+
+// data base connection
+const Connection=async()=>{
+    await mongoose.connect(process.env.URL,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true 
+   
+   }).then(()=>{
+    console.log("database connected")
+   }).catch((e)=>{
+    console.log(e.message)
+   })
+}
+
+Connection()
